b313e92b4dc7e41529498573f772055c4d1f056c
demguyz <sam_jarvi@hotmail.co.uk>
2015-10-19 18:25:48 +0100

    Sports Devil Update
    Insane Yo!

--- zips/plugin.video.SportsDevil/lib/utils/decryptionUtils.py
+++ zips/plugin.video.SportsDevil/lib/utils/decryptionUtils.py
@@ -1,13 +1,12 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 import pyDes
 import urllib
 import re
 
 from regexUtils import parseTextToGroups
-from webUtils import get_redirected_url
 
-from javascriptUtils import JsFunctions, JsUnpacker,JsUnpackerV2, JsUnwiser, JsUnIonCube, JsUnFunc, JsUnPP
+from javascriptUtils import JsFunctions, JsUnpacker, JsUnpackerV2, JsUnpacker95High, JsUnwiser, JsUnwiser2, JsUnIonCube, JsUnFunc, JsUnPP, JsUnPush
 
 
 def encryptDES_ECB(data, key):
@@ -16,6 +15,22 @@ def encryptDES_ECB(data, key):
     d = k.encrypt(data)
     assert k.decrypt(d, padmode=pyDes.PAD_PKCS5) == data
     return d
+
+def gAesDec(data, key):
+    import mycrypt
+    return mycrypt.decrypt(key,data)
+
+def aesDec(data, key):
+    from base64 import b64decode
+    try:
+        from Crypto.Cipher import AES
+    except ImportError:
+        import pyaes as AES
+    iv = 16 * '\x00'
+    cipher = AES.new(b64decode(key), AES.MODE_CBC, IV=iv)
+    padded_plaintext = cipher.decrypt(b64decode(data))
+    padding_len = ord(padded_plaintext[-1])
+    return padded_plaintext[:-padding_len]
 
 def encryptJimey(data):
     result = encryptDES_ECB(data,"PASSWORD").encode('base64').replace('/','').strip()
@@ -42,18 +57,23 @@ def ntos(n):
     return urllib.unquote(n)
 
 def doDemystify(data):
-
+    escape_again=False
+    
     #init jsFunctions and jsUnpacker
     jsF = JsFunctions()
     jsU = JsUnpacker()
     jsUV2 =JsUnpackerV2()
     jsUW = JsUnwiser()
+    jsUW2 = JsUnwiser2()
     jsUI = JsUnIonCube()
     jsUF = JsUnFunc()
     jsUP = JsUnPP()
+    jsU95 = JsUnpacker95High()
+    JsPush = JsUnPush()
 
     # replace NUL
     data = data.replace('\0','')
+
 
     # unescape
     r = re.compile('a1=["\'](%3C(?=[^\'"]*%\w\w)[^\'"]+)["\']')
@@ -74,7 +94,21 @@ def doDemystify(data):
         for g in r.findall(data):
             quoted=g
             data = data.replace(quoted, quoted.decode('unicode-escape'))
+            
+    r = re.compile('(eval\(decodeURIComponent\(atob\([\'"][^\'"]+[\'"]\)\)\);)')
+    while r.findall(data):
+        for g in r.findall(data):
+            r2 = re.compile('eval\(decodeURIComponent\(atob\([\'"]([^\'"]+)[\'"]\)\)\);')
+            for base64_data in r2.findall(g):
+                data = data.replace(g, urllib.unquote(base64_data.decode('base-64')))
        
+    r = re.compile('(base\([\'"]*[^\'"\)]+[\'"]*\))')
+    while r.findall(data):
+        for g in r.findall(data):
+            r2 = re.compile('base\([\'"]*([^\'"\)]+)[\'"]*\)')
+            for base64_data in r2.findall(g):
+                data = data.replace(g, urllib.unquote(base64_data.decode('base-64')))
+                escape_again=True
 
     # n98c4d2c
     if 'function n98c4d2c(' in data:
@@ -137,28 +171,27 @@ def doDemystify(data):
             for g in gs:
                 data = data.replace(g, destreamer(g))
 
-
-    # Tiny url
-    #r = re.compile('[\'"](http://(?:www.)?tinyurl.com/[^\'"]+)[\'"]',re.IGNORECASE + re.DOTALL)
-    #m = r.findall(data)
-    #if m:
-        #for tiny in m:
-            #data = data.replace(tiny, get_redirected_url(tiny))
-
-
     # JS P,A,C,K,E,D
     if jsU.containsPacked(data):
         data = jsU.unpackAll(data)
         
-    escape_again=False
+    
     #if still exists then apply v2
     if jsUV2.containsPacked(data):
         data = jsUV2.unpackAll(data)
+        escape_again=True
+        
+    if jsU95.containsPacked(data):
+        data = jsU95.unpackAll(data)
         escape_again=True
 
     # JS W,I,S,E
     if jsUW.containsWise(data):
         data = jsUW.unwiseAll(data)
+        escape_again=True
+        
+    if jsUW2.containsWise(data):
+        data = jsUW2.unwiseAll(data)
         escape_again=True
 
     # JS IonCube
@@ -174,15 +207,13 @@ def doDemystify(data):
     if jsUP.containUnPP(data):
         data = jsUP.UnPPAll(data)
         escape_again=True
+        
+    if JsPush.containUnPush(data):
+        data = JsPush.UnPush(data)
 
     # unescape again
     if escape_again:
-        r = re.compile('unescape\(\s*["\']([^\'"]+)["\']')
-        gs = r.findall(data)
-        if gs:
-            for g in gs:
-                quoted=g
-                data = data.replace(quoted, urllib.unquote_plus(quoted))            
+        data = doDemystify(data)
     return data
 
     
